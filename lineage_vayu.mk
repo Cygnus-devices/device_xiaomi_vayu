696a8e502eeda3a38547fde81c76e85621b15279
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 01:45:15 +0530

    vayu: Initial device tree makefiles

---
+++ lineage_vayu.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from vayu device
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="vayu"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
